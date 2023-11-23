7715ba00742abe1a296d7697a2f4e1b6e7c75214
Miloslav Nenadal <nenadalm@gmail.com>
2023-11-23 20:01:21 +0100

    Read bgg id from tlamagames.com

--- importer/src/app/tg.clj
+++ importer/src/app/tg.clj
@@ -1,7 +1,8 @@
 (ns app.tg
   (:require
    [clojure.string :as str]
-   [clojure.java.io])
+   [clojure.java.io]
+   [jsonista.core :as j])
   (:import
    [org.jsoup Jsoup]))
 
@@ -17,14 +18,21 @@
 (defn- doc->game-list [doc]
   (.select doc ".products-page > .product"))
 
-(defn- game->game-info [game]
-  {:name (str/trim (str/replace (.text (.select game ".name > span")) "(půjčovna)" ""))})
+(defn- game->game-info [game parameters]
+  (let [sku (.text (.select game "span[data-micro=sku]"))
+        bgg-id (str (get-in parameters [sku "bgg_id"]))]
+    {:name (str/trim (str/replace (.text (.select game ".name > span")) "(půjčovna)" ""))
+     :com.boardgamegeek.boardgame/id bgg-id}))
+
+(defn- get-parameters []
+  (get (j/read-value (java.net.URL. "https://tlama.fvstudio.cz/api/product/parameters")) "list"))
 
 (defn games []
-  (loop [url games-list-url
-         futures []]
-    (if url
-      (let [doc (game-list-doc url)]
-        (recur (doc->next-page doc)
-               (conj futures (future (pmap game->game-info (doc->game-list doc))))))
-      (into [] (mapcat deref) futures))))
+  (let [parameters (get-parameters)]
+    (loop [url games-list-url
+           futures []]
+      (if url
+        (let [doc (game-list-doc url)]
+          (recur (doc->next-page doc)
+                 (conj futures (future (pmap #(game->game-info % parameters) (doc->game-list doc))))))
+        (into [] (mapcat deref) futures)))))

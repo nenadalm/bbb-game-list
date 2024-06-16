e5866fc2f280ae10f6ac37e63c91044177c50c75
Miloslav Nenadal <nenadalm@gmail.com>
2024-06-16 14:41:37 +0200

    [importer] Fix mlp

--- importer/src/app/mlp.clj
+++ importer/src/app/mlp.clj
@@ -4,11 +4,20 @@
   (:import
    [org.jsoup Jsoup]))
 
-(def ^:private games-list-url "https://search.mlp.cz/cz/?action=c_s_ol&kde=all&query=&navigation=%2Bngeneric4%3A%5E%22hra%22%24n%24%20%2Bngeneric7%3A%5E%22P%22%24n%24")
+(def ^:private request-cookie-url "https://search.mlp.cz/cz/#/c_s_ol=")
+(def ^:private games-list-url "https://search.mlp.cz/cz/?action=c_s_ol&navigation=%2Bngeneric4%3A%5E%22hra%22%24n%24%20%2Bngeneric7%3A%5E%22P%22%24n%24")
 
-(defn- game-list-doc [url]
-  (with-open [xin (clojure.java.io/input-stream (java.net.URL. url))]
-    (Jsoup/parse xin "utf-8" url)))
+(defn- get-cookie []
+  (let [url (java.net.URL. request-cookie-url)
+        conn (.openConnection url)]
+    (.getHeaderField conn "Set-Cookie")))
+
+(defn- game-list-doc [cookie url]
+  (let [conn (.openConnection (java.net.URL. url))]
+    (.setRequestProperty conn "Cookie" cookie)
+    (.connect conn)
+    (with-open [xin (.getInputStream conn)]
+      (Jsoup/parse xin "utf-8" url))))
 
 (defn- doc->game-list [doc]
   (.select doc "#katalog-list .titul"))
@@ -17,11 +26,12 @@
   {:name (.text (.select game "a[itemprop=name]"))})
 
 (defn games []
-  (loop [offset 0
-         games []]
-    (let [url (str games-list-url "&offset=" offset)
-          games* (mapv game->game-info (doc->game-list (game-list-doc url)))
-          cnt (count games*)]
-      (if (< 0 cnt)
-        (recur (+ cnt offset) (into games games*))
-        games))))
+  (let [cookie (get-cookie)]
+    (loop [offset 0
+           games []]
+      (let [url (str games-list-url "&offset=" offset)
+            games* (mapv game->game-info (doc->game-list (game-list-doc cookie url)))
+            cnt (count games*)]
+        (if (< 0 cnt)
+          (recur (+ cnt offset) (into games games*))
+          games)))))

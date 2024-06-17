34ae9877daa8d857ef454e8ffb1287eceade80e9
Miloslav Nenadal <nenadalm@gmail.com>
2024-06-17 23:07:34 +0200

    [importer] Retry on Premature `EOF exception`, decrease bgg page size

--- importer/src/app/core.clj
+++ importer/src/app/core.clj
@@ -73,7 +73,7 @@
                            (comp
                             (map :com.boardgamegeek.boardgame/id)
                             (filter some?)
-                            (partition-all 100)
+                            (partition-all 80) ;; requests crash with `Premature EOF` exception with value `100`
                             (map (fn [ids]
                                    (bgg/games-details ids)))
                             cat
--- importer/src/app/url.clj
+++ importer/src/app/url.clj
@@ -10,16 +10,21 @@
 (def ^:private uncached-url-stream
   "boardgamegeek.com returns `429 Too Many Requests` in case we are too quick, sometimes it fails with eof error."
   (->> ->uncached-stream*
-       (u/retry 3 5000)
        (u/blocking-debounce 2000)))
+
+(defn- load-and-cache* [url]
+  (tap> (str "[GET] " url))
+  (with-open [xin (uncached-url-stream url)]
+    (url-cache/store url xin)
+    (clojure.java.io/input-stream (url-cache/load url))))
+
+(def ^:private load-and-cache
+  (->> load-and-cache*
+       (u/retry 3 5000)))
 
 (defn ->cached-stream [url]
   (if-let [x (url-cache/load url)]
     (do
       (tap> (str "[GET - CACHED] " url))
       (clojure.java.io/input-stream x))
-    (do
-      (tap> (str "[GET] " url))
-      (with-open [xin (uncached-url-stream url)]
-        (url-cache/store url xin)
-        (clojure.java.io/input-stream (url-cache/load url))))))
+    (load-and-cache url)))

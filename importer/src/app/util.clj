59b18b26dedeba748012ec8cde67687fd03b0952
Miloslav Nenadal <nenadalm@gmail.com>
2024-06-20 17:27:18 +0200

    [importer] Fix clj-kondo issues

--- importer/src/app/util.clj
+++ importer/src/app/util.clj
@@ -3,7 +3,7 @@
 (defn blocking-debounce
   "Returns function with same arguments as `f` that can be called at most once every `ms` ms. In case function would be called more times, current thread is blocked up to `ms` ms."
   [ms f]
-  (let [prev-end (atom (java.time.Instant/EPOCH))]
+  (let [prev-end (atom java.time.Instant/EPOCH)]
     (fn [& args]
       (let [start (java.time.Instant/now)
             pause-ms (.toMillis (java.time.Duration/between @prev-end start))]

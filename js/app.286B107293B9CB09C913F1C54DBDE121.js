662826e47721b3eeec9331440094926db858e658
nenadalm <nenadalm@gmail.com>
2024-06-20 15:20:03 +0000

    Deploying to gh-pages from @ nenadalm/bbb-game-list@da02e8671f0de56ced64186f5913912b3c23fe84 🚀

---
+++ js/app.286B107293B9CB09C913F1C54DBDE121.js
@@ -0,0 +1,4 @@
+(function(){
+'use strict';$APP.ma("app.core.init",function(){var f=navigator;f=null==f?null:f.serviceWorker;return null==f?null:f.register("worker.js")});
+}).call(this);
+//# sourceMappingURL=app.286B107293B9CB09C913F1C54DBDE121.js.map

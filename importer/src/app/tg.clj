a9301b039aaee32a580c7f78ded13e33946a874a
Miloslav Nenadal <nenadalm@gmail.com>
2024-10-09 19:54:55 +0200

    [importer] Retry tg

--- importer/src/app/tg.clj
+++ importer/src/app/tg.clj
@@ -2,15 +2,19 @@
   (:require
    [clojure.string :as str]
    [clojure.java.io]
-   [jsonista.core :as j])
+   [jsonista.core :as j]
+   [app.util :as u])
   (:import
    [org.jsoup Jsoup]))
 
 (def ^:private games-list-url "https://www.tlamagames.com/pujcovna/")
 
-(defn- game-list-doc [url]
+(defn- game-list-doc* [url]
   (with-open [xin (clojure.java.io/input-stream (java.net.URL. url))]
     (Jsoup/parse xin "utf-8" url)))
+
+(def ^:private game-list-doc
+  (u/retry 3 0 game-list-doc*))
 
 (defn- doc->next-page [doc]
   (not-empty (.attr (.select doc ".next.pagination-link") "abs:href")))
@@ -31,8 +35,11 @@
       languages (assoc :languages languages)
       bgg-id (assoc :com.boardgamegeek.boardgame/id bgg-id))))
 
-(defn- get-parameters []
+(defn- get-parameters* []
   (get (j/read-value (java.net.URL. "https://tlama.fvstudio.cz/api/product/parameters")) "list"))
+
+(def ^:private get-parameters
+  (u/retry 3 0 get-parameters*))
 
 (defn games []
   (let [parameters (get-parameters)]

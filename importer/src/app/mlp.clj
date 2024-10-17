a26cb9b001f51d02f67555c0fabf1ae7b770a039
Miloslav Nenadal <nenadalm@gmail.com>
2024-10-17 07:14:28 +0200

    [importer] Add retries for mlp

--- importer/src/app/mlp.clj
+++ importer/src/app/mlp.clj
@@ -1,23 +1,30 @@
 (ns app.mlp
   (:require
-   [clojure.java.io])
+   [clojure.java.io]
+   [app.util :as u])
   (:import
    [org.jsoup Jsoup]))
 
 (def ^:private request-cookie-url "https://search.mlp.cz/cz/#/c_s_ol=")
 (def ^:private games-list-url "https://search.mlp.cz/cz/?action=c_s_ol&navigation=%2Bngeneric4%3A%5E%22hra%22%24n%24%20%2Bngeneric7%3A%5E%22P%22%24n%24")
 
-(defn- get-cookie []
+(defn- get-cookie* []
   (let [url (java.net.URL. request-cookie-url)
         conn (.openConnection url)]
     (.getHeaderField conn "Set-Cookie")))
 
-(defn- game-list-doc [cookie url]
+(def ^:private get-cookie
+  (u/retry 3 0 get-cookie*))
+
+(defn- game-list-doc* [cookie url]
   (let [conn (.openConnection (java.net.URL. url))]
     (.setRequestProperty conn "Cookie" cookie)
     (.connect conn)
     (with-open [xin (.getInputStream conn)]
       (Jsoup/parse xin "utf-8" url))))
+
+(def ^:private game-list-doc
+  (u/retry 3 0 game-list-doc*))
 
 (defn- doc->game-list [doc]
   (.select doc "#katalog-list .titul"))
